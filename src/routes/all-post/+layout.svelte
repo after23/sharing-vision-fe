b0639968d7e4070ddf9957a79828e62c9dd25b9b
after23 <afiftrifarhan@gmail.com>
2023-07-14 22:23:12 +0700

    sphagetti code

---
+++ src/routes/all-post/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import 'iconify-icon'
+</script>
+
+<div class="container mx-auto">
+  <div class="grid grid-cols-3 gap-4 pt-4 pb-8">
+    <a href="/all-post/published" class="bg-gray-600 text-gray-300 text-center rounded-md py-2">
+      <h1>Published</h1>
+    </a>
+    <a href="/all-post/drafts" class="bg-gray-600 text-gray-300 text-center rounded-md py-2">
+      <h1>Drafts</h1>
+    </a>
+    <a href="/all-post/thrashed" class="bg-gray-600 text-gray-300 text-center rounded-md py-2">
+      <h1>Thrashed</h1>
+    </a>
+  </div>
+  <slot />
+</div>
---
+++ src/routes/all-post/published/+page.svelte
@@ -0,0 +1,29 @@
+ <script>
+	export let data;
+  const posts = data;
+</script>
+
+ 
+ <table class="table-auto mx-auto">
+  <thead>
+    <tr class="bg-gray-400">
+      <th class="pl-2 py-2 text-gray-100 rounded-l-md">Title</th>
+      <th class="px-2 py-2 text-gray-100">Category</th>
+      <th class="pr-2 py-2 text-gray-100 rounded-r-md">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each posts.data as post, i}
+      {#if post.status == "publish"}
+        <tr class:bg-gray-200={i%2==0}>
+          <td class="pl-2 py-2 text-center">{post.title}</td>
+          <td class="px-4 py-2 text-center">{post.category}</td>
+          <td class="pr-4 py-2 grid grid-cols-2 gap-1">
+            <a href="#"><iconify-icon class="text-amber-600 text-2xl hover:bg-gray-300" icon="lucide:edit"></iconify-icon></a>
+            <a href="#"><iconify-icon class="text-red-500 text-2xl hover:bg-gray-300" icon="ic:outline-delete"></iconify-icon></a>
+          </td>
+        </tr>
+      {/if}
+    {/each}
+  </tbody>
+</table>
